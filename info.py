f66096453f7ded53149abf3417e5f76497d7785e
Noob Developer <157978295+Geekyowner@users.noreply.github.com>
2024-09-15 11:57:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002070282646'))
 QR_CODE = environ.get('QR_CODE', 'https://graph.org/file/c74e1ee08ed0fed54d3ab.jpg')
-START_IMG = environ.get('START_IMG', 'https://graph.org/file/8df5fc1c665cf1a838856.jpg')
+START_IMG = environ.get('START_IMG', 'https://envs.sh/wK-.jpg')
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL','-1002236540396'))
 DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','-1002070282646'))
 STICKERS_IDS = ('CAACAgQAAxkBAAEK99dlfC7LDqnuwtGRkIoacot_dGC4zQACbg8AAuHqsVDaMQeY6CcRojME').split()
